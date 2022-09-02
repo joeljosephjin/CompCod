7c8dca2c25d5b5f3815757431260f1ad72a4edeb
Joel Joseph <34275997+joeljosephjin@users.noreply.github.com>
2022-09-02 10:49:36 +0530

    Update linked_list_utils.py

--- linked_list_utils.py
+++ linked_list_utils.py
@@ -1,18 +1,19 @@
+
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-def list_to_ll(lst=[1,2,3]):
+def lst_to_ll(lst=[1,2,3]):
     cur = dummy = ListNode()
     for e in lst:
         cur.next = ListNode(val=e, next=None)
         cur = cur.next
     return dummy.next
   
-dummy_ll = list_to_ll()
+dummy_ll = lst_to_ll()
 
-def ll_to_list(ll=dummy_ll):
+def ll_to_lst(ll=dummy_ll):
     lst = []
     while ll:
       lst.append(ll.val)
@@ -20,3 +21,8 @@ def ll_to_list(ll=dummy_ll):
     return lst
   
 dummy_lst = ll_to_lst()
+
+print(dummy_lst)
+
+if dummy_lst != [1,2,3]:
+    raise Exception("Something wrong!")
